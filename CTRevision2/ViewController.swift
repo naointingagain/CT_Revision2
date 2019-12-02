4374523e73a5cd8b98ddff9bd304f3681e4cf049
Koko <kokomoreno1010@gmail.com>
2019-12-02 14:44:30 +0800

    Initial Commit

---
+++ CTRevision2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTRevision2
+//
+//  Created by MAD2_P01 on 2/12/19.
+//  Copyright © 2019 Naomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
